df18b27a57546ba93d54d92438f7bf1ffb810169
Rawnak003 <shmrawnak@gmail.com>
2025-01-01 23:06:24 +0600

    Triangle Area

---
+++ bin/TriangleArea.dart
@@ -0,0 +1,11 @@
+main(){
+  double base = 10;
+  double height  = 15;
+  triangle(base,height);
+}
+
+triangle(double base, double height){
+  double result = 0.5*base*height;
+  print("The area of the Triangle is: $result");
+}
+
